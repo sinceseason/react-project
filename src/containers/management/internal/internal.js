2252bcb01ce255ca169ab73db8dca866bcd93da7
sinceseason <1766578925@qq.com>
2018-06-29 17:20:12 +0800

    modal

--- src/containers/management/internal/internal.js
+++ src/containers/management/internal/internal.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { EditableTable } from './editableTable';
 
 export class Internal extends React.Component {
     render() {
         return (
-            <div className="main-container">internal</div>
+            <div className="main-container">
+                <EditableTable />
+            </div>
         )
     }
 }
--- src/containers/management/wharf/wharf.js
+++ src/containers/management/wharf/wharf.js
@@ -1,31 +1,32 @@
 import React from 'react';
 import { I18n } from 'react-i18next';
-import { Row, Col, Button, Table } from 'antd';
+import { Row, Col, Button, Table, Modal } from 'antd';
 import Http from '../../../http/http';
 import * as CONSTANT from '../../../const/constant';
-
-const columns = [{
-    title: '序号',
-    dataIndex: 'id',
-    // key: 'id'
-}, {
-    title: '码头名称',
-    dataIndex: 'wharfName',
-    // key: 'wharfName'
-},{
-    title: '操作',
-    // key: 'action'
-}];
 
 export class Wharf extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             wharfList: [],
-        }
+            visible: false,
+        };
+        this.columns = [{
+            title: '序号',
+            dataIndex: 'id',
+        }, {
+            title: '码头名称',
+            dataIndex: 'wharfName',
+        },{
+            title: '操作',
+        }];
     }
 
     componentWillMount() {
+        this.queryWharf();
+    }
+
+    queryWharf() {
         Http._Get('wharf')
             .then(data => {
                 let result = data.data;
@@ -38,17 +39,49 @@ export class Wharf extends React.Component {
             })
     }
 
+    showModal = () => {
+        this.setState({
+          visible: true,
+        });
+      }
+    handleOk = (e) => {
+        console.log(e);
+        this.setState({
+          visible: false,
+        });
+      }
+      handleCancel = (e) => {
+        console.log(e);
+        this.setState({
+          visible: false,
+        });
+      }
+
     render() {
         return (
             <I18n ns="translate">
             {(t) => (
             <div className="main-container">
                 <Row className="bar">
-                    <Button type="primary">{t('button.save')}</Button>
+                    {/* QUESTION: 为什么这里写为 onClick={() => this.showModal} 就无法触发 Model ? */}
+                    <Button type="primary" onClick={this.showModal}>{t('button.save')}</Button>
                 </Row>
                 <div className="context">
-                    <Table rowKey="id" columns={columns} dataSource={this.state.wharfList}></Table>
+                    <Table rowKey="id" columns={this.columns} dataSource={this.state.wharfList}></Table>
                 </div>
+                <Modal
+                  title={t('button.add')}
+                  wrapClassName="vertical-center-modal"
+                  maskStyle={{}}
+                  bodyStyle={{textAlign: 'center'}}
+                  visible={this.state.visible}
+                  onOk={this.handleOk}
+                  onCancel={this.handleCancel}
+                >
+                  <p>Some contents...</p>
+                  <p>Some contents...</p>
+                  <p>Some contents...</p>
+                </Modal>
             </div>
             )}
             </I18n>
